c49e80e6f347c5a58ab391ccdb387f46e57e6f2c
kwj52321 <kwj5232@snu.ac.kr>
2026-04-28 07:37:35 +0900

    Time: 51 ms (82.42%), Space: 24.8 MB (49.68%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,30 +1,33 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        if len(points) == 1:
-            return points
 
-        mid = len(points) // 2
-        left = self.kClosest(points[:mid], k)
-        right = self.kClosest(points[mid:], k)
-        merge = []
-        i = j = 0
-        while i<len(left) and j<len(right):
-            l_dist = left[i][0]**2 + left[i][1]**2
-            r_dist = right[j][0]**2 + right[j][1]**2
-            if(l_dist <= r_dist):
-                merge.append(left[i])
-                i += 1
-            else:
-                merge.append(right[j])
-                j += 1
-        if i < len(left):
-            merge.extend(left[i:])
-            return merge[:k]
-        elif j < len(right):
-            merge.extend(right[j:])
-            return merge[:k]
-        else: 
-            return merge[:k]
+        import heapq
+        return heapq.nsmallest(k, points, key = lambda x: x[0]**2 + x[1]**2)
+        # if len(points) == 1:
+        #     return points
+
+        # mid = len(points) // 2
+        # left = self.kClosest(points[:mid], k)
+        # right = self.kClosest(points[mid:], k)
+        # merge = []
+        # i = j = 0
+        # while i<len(left) and j<len(right):
+        #     l_dist = left[i][0]**2 + left[i][1]**2
+        #     r_dist = right[j][0]**2 + right[j][1]**2
+        #     if(l_dist <= r_dist):
+        #         merge.append(left[i])
+        #         i += 1
+        #     else:
+        #         merge.append(right[j])
+        #         j += 1
+        # if i < len(left):
+        #     merge.extend(left[i:])
+        #     return merge[:k]
+        # elif j < len(right):
+        #     merge.extend(right[j:])
+        #     return merge[:k]
+        # else: 
+        #     return merge[:k]
 
 
         
